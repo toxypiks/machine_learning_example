e7fd75dc82f559fe732ff60e91b748a8a44d7672
toxypiks <HannaHinkelbein89@gmx.de>
2024-11-05 13:56:50 +0100

    Introduce bias to model

--- src/main.c
+++ src/main.c
@@ -18,12 +18,13 @@ float rand_float(void)
   return (float) rand()/ (float)RAND_MAX;
 }
 
-float cost(float w)
+float cost(float w, float b)
 {
   float result = 0.0f;
   for(size_t i = 0; i < train_count; ++i) {
 	float x = train[i][0];
-	float y = x*w;
+	//model (y = x*w) of single neuron with single connection, whereas w = weight plus bias b
+	float y = x*w + b;
 	float d = y - train[i][1];
 	// d*d cause it amplifies errors and let result always be positive
 	result += d*d;
@@ -39,18 +40,22 @@ int main() {
   // create model that predicts the desired outcome of training data (number two of array entity)
   // y = x*w
   float w = rand_float() * 10.0f;
+  float b = rand_float() * 5.0f;
 
   float eps = 1e-3;
   float rate = 1e-3;
   //calc easy form of derivative of function (finite difference)
 
   for(size_t i = 0; i < 500; ++i)  {
-   float dcost = (cost(w + eps) - cost(w))/eps;
-   w -= rate * dcost;
-   printf("%f\n", cost(w));
+   float c = cost(w, b);
+   float dw = (cost(w + eps, b) - c)/eps;
+   float db = (cost(w, eps + b) - c)/eps;
+   w -= rate * dw;
+   b -= rate * db;
+   printf("cost: %f, w = %f, b = %f\n", cost(w, b), w, b);
   }
 
   printf("-----------------------\n");
-  printf("model value: %f\n", w);
+  printf("w: %f, b: %f\n", w, b);
   return 0;
 }
