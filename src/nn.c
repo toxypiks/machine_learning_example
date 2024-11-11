7c68966f1dd08a099b47b5f6b6cbe2eb4ef835bd
toxypiks <HannaHinkelbein89@gmx.de>
2024-11-11 15:44:37 +0100

    Pass data through second layer

--- src/nn.c
+++ src/nn.c
@@ -15,12 +15,15 @@ int main(void)
   // bias of first layer
   Mat b1 = mat_alloc(1, 2);
 
-  // result matrix
+  // result matrix first layer
   Mat a1 = mat_alloc(1, 2);
 
   // second layer
   Mat w2 = mat_alloc(2, 1);
   Mat b2 = mat_alloc(1, 1);
+
+  // result matrix second layer
+  Mat a2 = mat_alloc(1, 2);
 
   mat_rand(w1, 0, 1);
   mat_rand(b1, 0, 1);
@@ -29,12 +32,20 @@ int main(void)
 
   // forwarding data to next layer
   // x = {0, 1};
-  MAT_AT(x, 0, 0) = 0;
-  MAT_AT(x, 0, 1) = 1;
+  float x1 = 0;
+  float x2 = 1;
+  MAT_AT(x, 0, 0) = x1;
+  MAT_AT(x, 0, 1) = x2;
 
   mat_dot(a1, x, w1);
   mat_sum(a1, b1);
   mat_sig(a1);
+
+  mat_dot(a2, a1, w2);
+  mat_sum(a2, b2);
+  mat_sig(a2);
+
+  float y = *a2.es;
 
   // sigmoid(x*w1 + b1)
 
