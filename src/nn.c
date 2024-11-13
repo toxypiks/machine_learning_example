ea0fc320f6f9ac45f1a4606e42f78bc5adc8b987
toxypiks <HannaHinkelbein89@gmx.de>
2024-11-13 11:33:21 +0100

    Create xor model with deep learning library

--- src/nn.c
+++ src/nn.c
@@ -3,56 +3,57 @@
 #include <time.h>
 #include "nn.h"
 
+typedef struct {
+ // input data
+  Mat a0;
+  // first layer
+  Mat w1, b1, a1;
+  //second layer
+  Mat w2, b2, a2;
+} Xor;
+
+float forward_xor(Xor m, float x1, float x2) {
+  MAT_AT(m.a0, 0, 0) = x1;
+  MAT_AT(m.a0, 0, 1) = x2;
+
+  mat_dot(m.a1, m.a0, m.w1);
+  mat_sum(m.a1, m.b1);
+  mat_sig(m.a1);
+
+  mat_dot(m.a2, m.a1, m.w2);
+  mat_sum(m.a2, m.b2);
+  mat_sig(m.a2);
+
+  return *m.a2.es;
+}
+
 int main(void)
 {
   srand(time(0));
 
-  // input data
-  Mat x = mat_alloc(1, 2);
+  Xor m;
 
-  // weight of first layer
-  Mat w1 = mat_alloc(2, 2);
-  // bias of first layer
-  Mat b1 = mat_alloc(1, 2);
+  m.a0 = mat_alloc(1, 2);
 
-  // result matrix first layer
-  Mat a1 = mat_alloc(1, 2);
+  m.w1 = mat_alloc(2, 2);
+  m.b1 = mat_alloc(1, 2);
+  m.a1 = mat_alloc(1, 2);
 
-  // second layer
-  Mat w2 = mat_alloc(2, 1);
-  Mat b2 = mat_alloc(1, 1);
+  m.w2 = mat_alloc(2, 1);
+  m.b2 = mat_alloc(1, 1);
+  m.a2 = mat_alloc(1, 1);
 
-  // result matrix second layer
-  Mat a2 = mat_alloc(1, 2);
+  mat_rand(m.w1, 0, 1);
+  mat_rand(m.b1, 0, 1);
+  mat_rand(m.w2, 0, 1);
+  mat_rand(m.b2, 0, 1);
 
-  mat_rand(w1, 0, 1);
-  mat_rand(b1, 0, 1);
-  mat_rand(w2, 0, 1);
-  mat_rand(b2, 0, 1);
-
-  // forwarding data to next layer
-  // x = {0, 1};
-  float x1 = 0;
-  float x2 = 1;
-  MAT_AT(x, 0, 0) = x1;
-  MAT_AT(x, 0, 1) = x2;
-
-  mat_dot(a1, x, w1);
-  mat_sum(a1, b1);
-  mat_sig(a1);
-
-  mat_dot(a2, a1, w2);
-  mat_sum(a2, b2);
-  mat_sig(a2);
-
-  float y = *a2.es;
-
-  // sigmoid(x*w1 + b1)
-
-  MAT_PRINT(w1);
-  MAT_PRINT(b1);
-  MAT_PRINT(w2);
-  MAT_PRINT(b2);
+  // create truth-table to see performance
+  for(size_t i = 0; i < 2; ++i) {
+	for (size_t j = 0; j < 2; ++j) {
+	  printf("%zu ^ %zu = %f\n", i, j, forward_xor(m, i, j));
+	}
+  }
 
   return 0;
 }
