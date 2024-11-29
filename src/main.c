ff25232b1401b63819114660dd14be11775b20b2
toxypiks <HannaHinkelbein89@gmx.de>
2024-11-29 10:47:51 +0100

    Use actuall derivative rather than finite difference to improve cost function

--- src/main.c
+++ src/main.c
@@ -18,44 +18,57 @@ float rand_float(void)
   return (float) rand()/ (float)RAND_MAX;
 }
 
-float cost(float w, float b)
+float cost(float w)
 {
   float result = 0.0f;
   for(size_t i = 0; i < train_count; ++i) {
 	float x = train[i][0];
-	//model (y = x*w) of single neuron with single connection, whereas w = weight plus bias b
-	float y = x*w + b;
+	float y = x*w;
 	float d = y - train[i][1];
-	// d*d cause it amplifies errors and let result always be positive
 	result += d*d;
   }
   return result /= train_count;
 }
 
+float dcost(float w)
+{
+  float result = 0.0f;
+  size_t n = train_count;
+  for (size_t i = 0; i < n; ++i) {
+	float x = train[i][0];
+	float y = train[i][1];
+	result += 2*(x*w - y)*x;
+  }
+  return result /= n;
+}
 
 int main() {
 
-  //seed random number generator with time
-  srand(time(0));
+  // seed random number generator with time
+  // srand(time(0));
+  srand(69);
   // create model that predicts the desired outcome of training data (number two of array entity)
   // y = x*w
   float w = rand_float() * 10.0f;
-  float b = rand_float() * 5.0f;
 
-  float eps = 1e-3;
-  float rate = 1e-3;
-  //calc easy form of derivative of function (finite difference)
+  float rate = 1e-1;
+  // calc easy form of derivative of function (finite difference)
 
-  for(size_t i = 0; i < 500; ++i)  {
-   float c = cost(w, b);
-   float dw = (cost(w + eps, b) - c)/eps;
-   float db = (cost(w, eps + b) - c)/eps;
-   w -= rate * dw;
-   b -= rate * db;
-   printf("cost: %f, w = %f, b = %f\n", cost(w, b), w, b);
-  }
+  printf("cost: %f, w = %f\n", cost(w), w);
+  for(size_t i = 0; i < 10; ++i)  {
+// implements ability to flip flop between finite diff and derivative (if 0 -> derivative, if 1 -> finite diff)
+#if 0
+    float eps = 1e-3;
+	float c = cost(w);
+	float dw = (cost(w + eps) - c)/eps;
+#else
+    float dw = dcost(w);
+#endif
+    w -= rate * dw;
+    printf("cost: %f, w = %f\n", cost(w), w);
+ }
 
-  printf("-----------------------\n");
-  printf("w: %f, b: %f\n", w, b);
-  return 0;
+ printf("-----------------------\n");
+ printf("w: %f\n", w);
+ return 0;
 }
