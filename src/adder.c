c4adacae73c75b1a65126e7144e7705316e0bee8
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-05 13:22:01 +0100

    Generate matrix with all possible 0 and 1 combinations depending on matrix size

--- src/adder.c
+++ src/adder.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#define NN_IMPLEMENTATION
+#include "nn.h"
+
+#define BITS 2
 
 int main(void) {
-  printf("hello\n");
+
+  // left shift operator 1 << 2 = (1*2)*2 = 4 => 4 * 4 = 16 rows
+  // input are two numbers with 2 bits each at the beginning
+  // BITS will get increased in the future
+  size_t n = (1<<BITS);
+  size_t rows = n*n;
+  Mat ti = mat_alloc(rows, 2*BITS);
+  for (size_t i = 0; i < ti.rows; ++i) {
+	size_t x = i/n;
+	size_t y = i%n;
+	for (size_t j = 0; j < BITS; ++j) {
+	  MAT_AT(ti, i, j) = (x>>j)&1;
+	  MAT_AT(ti, i, j + BITS) = (y>>j)&1;
+	}
+  }
+  MAT_PRINT(ti);
+
   return 0;
 }
