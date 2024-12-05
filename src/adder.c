ddacd711b8043693fb87703c84e2c6d2694a1d57
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-05 13:45:44 +0100

    Generate output with all possible 0 and 1 combinations

--- src/adder.c
+++ src/adder.c
@@ -11,15 +11,24 @@ int main(void) {
   size_t n = (1<<BITS);
   size_t rows = n*n;
   Mat ti = mat_alloc(rows, 2*BITS);
+  Mat to = mat_alloc(rows, BITS + 1);
   for (size_t i = 0; i < ti.rows; ++i) {
-	size_t x = i/n;
-	size_t y = i%n;
-	for (size_t j = 0; j < BITS; ++j) {
-	  MAT_AT(ti, i, j) = (x>>j)&1;
-	  MAT_AT(ti, i, j + BITS) = (y>>j)&1;
-	}
+    size_t x = i/n;
+    size_t y = i%n;
+    size_t z = x + y;
+    size_t overflow = z >= n;
+    for (size_t j = 0; j < BITS; ++j) {
+      MAT_AT(ti, i, j) = (x>>j)&1;
+      MAT_AT(ti, i, j + BITS) = (y>>j)&1;
+      if (overflow) {
+        MAT_AT(to, i, j) = 0;
+      } else {
+        MAT_AT(to, i, j) = (z>>j)&1;
+      }
+    }
+    MAT_AT(to, i, BITS) = overflow;
   }
   MAT_PRINT(ti);
-
+  MAT_PRINT(to);
   return 0;
 }
