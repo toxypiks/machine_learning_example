9f1f9a71e5c2290415b509b2cf71718f467fc8b6
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-11 14:32:09 +0100

    Use backpropagation for learning in neural network

--- src/adder.c
+++ src/adder.c
@@ -1,7 +1,7 @@
 #define NN_IMPLEMENTATION
 #include "nn.h"
 
-#define BITS 2
+#define BITS 3
 
 int main(void) {
   // left shift operator 1 << 2 = (1*2)*2 = 4 => 4 * 4 = 16 rows
@@ -15,29 +15,57 @@ int main(void) {
     size_t x = i/n;
     size_t y = i%n;
     size_t z = x + y;
-    size_t overflow = z >= n;
     for (size_t j = 0; j < BITS; ++j) {
       MAT_AT(ti, i, j) = (x>>j)&1;
       MAT_AT(ti, i, j + BITS) = (y>>j)&1;
-      if (overflow) {
-        MAT_AT(to, i, j) = 0;
-      } else {
-        MAT_AT(to, i, j) = (z>>j)&1;
+      MAT_AT(to, i, j) = (z>>j)&1;
       }
+    MAT_AT(to, i, BITS) = z >= n;
     }
-    MAT_AT(to, i, BITS) = overflow;
-  }
-  MAT_PRINT(ti);
-  //MAT_PRINT(to);
 
-  size_t arch[] = {2*BITS, BITS + 1};
+  size_t arch[] = {2*BITS, 2*BITS + 1, BITS + 1};
   NN nn = nn_alloc(arch, ARRAY_LEN(arch));
   NN g = nn_alloc(arch, ARRAY_LEN(arch));
   nn_rand(nn, 0, 1);
-  NN_PRINT(nn);
-  //nn_backprop(nn, g, ti, to);
-  NN_PRINT(g);
+  //NN_PRINT(nn);
 
+  float rate = 1;
   printf("cost : %f\n", nn_cost(nn, ti, to));
+
+  for(size_t i = 0; i < 10*1000; ++i) {
+	nn_backprop(nn, g, ti, to);
+	nn_apply_finite_diff(nn, g, rate);
+	printf("%zu: c = %f\n", i, nn_cost(nn, ti, to));
+  }
+
+  size_t fails = 0;
+  for (size_t x = 0; x < n; ++x) {
+    for (size_t y = 0; y < n; ++y) {
+      size_t z = x + y;
+      for (size_t j = 0; j < BITS; ++j) {
+        MAT_AT(NN_INPUT(nn), 0, j) = (x>>j)&1;
+        MAT_AT(NN_INPUT(nn), 0, j + BITS) = (y>>j)&1;
+      }
+      nn_forward(nn);
+      if (MAT_AT(NN_OUTPUT(nn), 0, BITS) > 0.5f) {
+        if (z < n) {
+          printf("%zu + %zu = (OVERFLOW<>%zu)\n", x, y, z);
+          fails += 1;
+        }
+      } else {
+        size_t a = 0;
+        for (size_t j = 0; j < BITS; ++j) {
+          size_t bit = MAT_AT(NN_OUTPUT(nn), 0, j) > 0.5f;
+          a |= bit<<j;
+        }
+        if (z != a) {
+          printf("%zu + %zu = (%zu<>%zu)\n", x, y, z, a);
+          fails += 1;
+        }
+      }
+    }
+  }
+  if (fails == 0) printf("OK\n");
+
   return 0;
 }
