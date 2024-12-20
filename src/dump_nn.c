fb43abeee6407df4ab148cef7cab3d7394be7fe6
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-20 15:16:59 +0100

    Create red rectangle

--- src/dump_nn.c
+++ src/dump_nn.c
@@ -19,7 +19,15 @@ int main (void)
   NN_PRINT(nn);
 
   Olivec_Canvas img = olivec_canvas(img_pixels, IMG_WIDTH, IMG_HEIGHT, IMG_WIDTH);
+  olivec_fill(img, 0xFF0000FF);
 
-  olivec_fill(img, 0xFF000FF);
+  const char *img_file_path = "nn.png";
+  if (!stbi_write_png(img_file_path, img.width, img.height, 4, img.pixels, img.stride*sizeof(uint32_t)))
+  {
+    printf("ERROR: could not save file %s\n", img_file_path);
+    return 1;
+  }
+  printf("Saved NN to %s\n", img_file_path);
+
   return 0;
 }
