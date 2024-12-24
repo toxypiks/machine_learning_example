7db88748905aa570e0902dc3cf0566c4ef247c4f
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-24 16:28:11 +0100

    Visualize 2 neurons

--- src/dump_nn.c
+++ src/dump_nn.c
@@ -18,8 +18,24 @@ int main (void)
   nn_rand(nn, 0, 1);
   NN_PRINT(nn);
 
+  uint32_t neuron_color = 0xFF0000FF;
+  uint32_t background_color = 0xFF181818;
   Olivec_Canvas img = olivec_canvas(img_pixels, IMG_WIDTH, IMG_HEIGHT, IMG_WIDTH);
-  olivec_fill(img, 0xFF0000FF);
+  olivec_fill(img, background_color);
+
+  size_t layer_count = arch[0];
+  int neuron_radius = 25;
+  int layer_border_pad = 50;
+
+  int layer_height = img.height - 2*layer_border_pad;
+  int layer_vpad = layer_height/layer_count;
+  int layer_x = img.width/2;
+  int layer_y = img.height/2 - layer_height/2;
+  for (size_t i = 0; i < arch[0]; ++i) {
+    int cx = layer_x;
+    int cy = layer_y + i * layer_vpad;
+    olivec_circle(img, cx, cy, neuron_radius, neuron_color);
+  }
 
   const char *img_file_path = "nn.png";
   if (!stbi_write_png(img_file_path, img.width, img.height, 4, img.pixels, img.stride*sizeof(uint32_t)))
